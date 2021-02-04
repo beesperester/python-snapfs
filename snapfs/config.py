79e9ddd62eca8913ad54e2a7ed4194cdde241ebf
Bernhard Esperester <bernhard@esperester.de>
2021-02-04 15:08:22 +0100

    implements basic snap functionality

--- snapfs/config.py
+++ snapfs/config.py
@@ -2,12 +2,10 @@ class Config:
 
     def __init__(self) -> None:
         self.ignore = [
-            "*.DS_Store",
-            "*.git",
-            "*.venv",
-            "*.vscode",
-            "*.pyc",
-            "__pycache__"
+            ".snapfs",
+            "*",
+            "^*.c4d",
+            "^*.png"
         ]
 
 
